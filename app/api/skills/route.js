a6d9a7ee8e73c71a17b29e4761a55bed45444455
ojus soni <ojusoni@gmail.com>
2023-12-11 09:57:26 +0530

    updating files

--- app/api/skills/route.js
+++ app/api/skills/route.js
@@ -5,9 +5,13 @@ import clientPromise from "@/db/database";
 // To handle a GET request to /api
 export async function GET(request) {
   // Do whatever you want
-  const client = await clientPromise;
-  const db = client.db("portfolio_data");
-  const allPosts = await db.collection("skills").find({}).toArray();
-  return NextResponse.json({allPosts });
+  try {
+    const client = await clientPromise;
+    const db = client.db("portfolio_data");
+    const allPosts = await db.collection("skills").find({}).toArray();
+    return NextResponse.json({allPosts });
+  } catch (error) {
+      return NextResponse.json({error:error})
+  }
 }
 
